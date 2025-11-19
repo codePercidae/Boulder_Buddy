5c927e34246d6427e9489928cbcc628f57f03115
Tommi Kiiski <kiiski.tommi@hotmail.com>
2025-11-19 20:17:36 +0200

    Clean up

--- gyms.py
+++ gyms.py
@@ -4,7 +4,8 @@ def get_gyms():
     return db.query_all("SELECT * FROM gyms")
 
 def get_favourite_gym(user_id):
-    db.query_all("""SELECT MAX(route_by_gym) 
+    return db.query_all("""SELECT MAX(route_by_gym) 
         FROM (SELECT COUNT(*) route_by_gym FROM routes, climbed 
         WHERE routes.id = climbed.route_id 
-        AND climbed.user_id = (?) GROUP BY routes.gym_id)""", [user_id])[0]["MAX(route_by_gym)"]
+        AND climbed.user_id = (?) GROUP BY routes.gym_id)""",
+        [user_id])[0]["MAX(route_by_gym)"]
